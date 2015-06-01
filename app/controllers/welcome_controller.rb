1a26b4bbb5ade8c1a532c07d1c306aab5aceca9e
Michael Westbom <totallymike@gmail.com>
2015-06-01 13:38:02 -0400

    Put guestbook right in welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,21 @@
 class WelcomeController < ApplicationController
   def index
+    @guestbook = Guestbook.all
+    @new_guest = Guestbook.new
+  end
+
+  def sign
+    guest = Guestbook.new(guestbook_params)
+    if guest.save
+      redirect_to welcome_index_path
+    else
+      redirect_to welcome_index_path
+    end
+  end
+
+  private
+
+  def guestbook_params
+    params.require(:guestbook).permit(:username, :body)
   end
 end
